97bb54d4906a7f5c59ca579ffab97844099e36f6
JM297 <sirweegee777@gmail.com>
2020-04-18 14:09:33 -0400

    Initial Upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GoogleMapReact from 'google-map-react';
+import Circle from './Circle'
 
-function App() {
+export default function App() {
+
+  const [center, setCenter] = React.useState([39.7741143, -86.1761807]);
+  const [value, setValues] = React.useState({
+    x: "",
+    y: ""
+  });
+
+  const handleChange = prop => event => {
+    setValues({ ...value, [prop]: event.target.value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" style={{ textAlign: 'center' }}>
+      <button onClick={() => setCenter([42.652354, -73.755955])} >Albany, NY</button>
+      <button onClick={() => setCenter([47.605884, -122.334562])} >Seattle, WA</button>
+      <button onClick={() => setCenter([42.281116, -83.743426])} >Ann Arbor, MI</button><br />
+      <button onClick={() => setCenter([39.7741143, -86.1761807])} >Reset</button>
+      <div style={{ height: '600px', width: '600px', margin: 'auto' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyDr-7s3eGcpdT_keRKxHfJmQnJCj0Ia6jw" }}
+          center={center}
+          defaultZoom={15}
         >
-          Learn React
-        </a>
-      </header>
+          <Circle lat={center[0]} lng={center[1]} />
+        </GoogleMapReact>
+      </div>
+      <input onChange={handleChange("x")} placeholder={"Latitude"} />
+      <input onChange={handleChange("y")} placeholder={"Longitude"} /><br />
+      <button onClick={() => setCenter([Number(value.x), Number(value.y)])} >Warp to Location</button>
     </div>
   );
 }
-
-export default App;
---
+++ src/Circle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Circle(props){
+    const style = {backgroundColor:'red',height:'15px', width:'15px',borderRadius:'15px'}
+    return(
+        <div style={style}>
+
+        </div>
+    )
+}
